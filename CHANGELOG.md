47fa3a355cc52e7b3eed4729b71877990a461b25
Bernard Maltais <bmaltais@gmail.com>
2020-06-29 17:21:56 +0000

    Make output return null when deploy is false

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
+## v1.0.1 (June 2020)
+
+FEATURES: 
+* **new feature:**  Make output return null when deploy is false
+
+IMPROVEMENTS:
+
+BUGS:
+
+
 ## v1.0.0 (June 2020)
 
 FEATURES: 
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,4 @@
 output "network_ddos_protection_plan" {
-  depends_on = [azurerm_network_ddos_protection_plan.network_ddos_protection_plan]
-
   description = "Returns the full set of network_ddos_protection_plan created"
-  value = azurerm_network_ddos_protection_plan.network_ddos_protection_plan
+  value = var.deploy ? azurerm_network_ddos_protection_plan.network_ddos_protection_plan[0] : null
 }
