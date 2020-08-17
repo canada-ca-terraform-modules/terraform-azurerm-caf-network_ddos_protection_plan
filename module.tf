a17ca7de6a5a3a5513057feb0f9a1d88b1818c4d
Bernard Maltais <bmaltais@gmail.com>
2020-08-17 12:46:18 +0000

    Update code for terraform 0.13

--- module.tf
+++ module.tf
@@ -1,5 +1,4 @@
 resource azurerm_network_ddos_protection_plan network_ddos_protection_plan {
-  count               = var.deploy ? 1 : 0
   name                = local.name
   location            = var.resource_group.location
   resource_group_name = var.resource_group.name
